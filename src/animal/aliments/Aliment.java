5e9365fb00b42dfe87fcaf511f0100e43802d199
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-12 13:48:32 +0200

    Commit initial

---
+++ src/animal/aliments/Aliment.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animal.aliments;
+
+/**
+ *
+ * @author diginamic05
+ */
+public class Aliment {
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+    
+    private String type;
+    public Aliment(String t)
+    {
+        this.type = t;
+    }
+}
